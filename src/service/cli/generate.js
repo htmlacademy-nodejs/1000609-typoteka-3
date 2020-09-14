b76c91861d4f04e2bd944c72a9de923c068b7b0b
Ilya Volkov <volkov-ilyha@ya.ru>
2020-09-14 22:55:09 +0300

    📝 take out mock data

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -8,74 +8,27 @@ const {getRandomInt, getRandomDate, shuffleAndSlice} = require(`../../utils.js`)
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`
-];
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
-const SENTENCES = [
-  `Ёлки — это не просто красивое дерево.`,
-  `Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего.`,
-  `Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка.`,
-  `Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно.`,
-  `Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно.`,
-  `Просто действуйте.`,
-  `Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания.`,
-  `Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами.`,
-  `Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать?`,
-  `Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи.`,
-  `Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года.`,
-  `Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`
-];
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
 
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`
-];
-
-const generateMockData = (count) => (
+const generateMockData = (count, titles, sentences, categories) => (
   Array.from({length: count}, () => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     createdDate: getRandomDate(),
-    announce: shuffleAndSlice(SENTENCES, 5).join(` `),
-    fullText: shuffleAndSlice(SENTENCES).join(` `),
-    category: shuffleAndSlice(CATEGORIES)
+    announce: shuffleAndSlice(sentences, 5).join(` `),
+    fullText: shuffleAndSlice(sentences).join(` `),
+    category: shuffleAndSlice(categories)
   }))
 );
 
@@ -83,13 +36,16 @@ module.exports = {
   name: `--generate`,
   async run(args) {
     const count = Number.parseInt(args[0], 10) || DEFAULT_COUNT;
+    const titles = await readContent(FILE_TITLES_PATH);
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
 
     if (count > 1000) {
       console.error(chalk.red(`Не больше 1000 публикаций`));
       process.exit(ExitCode.ERROR);
     }
 
-    const content = JSON.stringify(generateMockData(count));
+    const content = JSON.stringify(generateMockData(count, titles, sentences, categories));
 
     try {
       await fs.writeFile(FILE_NAME, content);
