e9bf588360a2f47a36b9e49eb72b2ed43af5647e
Ilya Volkov <volkov-ilyha@ya.ru>
2020-09-20 13:15:03 +0300

    ⚠ add http status codes to constants

--- src/constants.js
+++ src/constants.js
@@ -6,11 +6,17 @@ const USER_ARGV_INDEX = 2;
 
 const ExitCode = {
   ERROR: 1,
-  SUCCESS: 0,
+  SUCCESS: 0
+};
+
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404
 };
 
 module.exports = {
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
-  ExitCode
+  ExitCode,
+  HttpCode
 };
