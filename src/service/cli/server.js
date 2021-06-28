8841bc3f4c4eea452c0ac35d26e09780d0f415d1
Ilya Volkov <volkov-ilyha@ya.ru>
2021-06-28 13:40:40 +0300

    🔄 updating server.js

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,32 +1,18 @@
 'use strict';
 
 const express = require(`express`);
-const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
-const {HttpCode} = require(`../../constants`);
-const {Router} = express;
+const {HttpCode, API_PREFIX} = require(`../../constants`);
+const routes = require(`../api`);
 
 const DEFAULT_PORT = 3000;
-const FILE_NAME = `mocks.json`;
-
-const postsRouter = new Router();
-postsRouter.get(`/`, async (req, res) => {
-  try {
-    const fileContent = await fs.readFile(FILE_NAME);
-    const mocks = JSON.parse(fileContent);
-    res.json(mocks);
-  } catch (err) {
-    res.json([]);
-  }
-});
 
 const app = express();
 app.use(express.json());
-app.use(`/posts`, postsRouter);
+app.use(API_PREFIX, routes);
 app.use((req, res) => res
   .status(HttpCode.NOT_FOUND)
   .send(`Not found`));
-
 
 module.exports = {
   name: `--server`,
