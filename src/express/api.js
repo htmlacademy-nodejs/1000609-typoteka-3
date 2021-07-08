51bc33ad49abdf7205e5fdd53ca759ddc24775d0
Ilya Volkov <volkov-ilyha@ya.ru>
2021-07-08 17:53:27 +0300

    🗝 creating api.js for front-server

---
+++ src/express/api.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const axios = require(`axios`);
+
+const TIMEOUT = 1000;
+
+const port = process.env.API_PORT || 3000;
+const defaultURL = `http://localhost:${port}/api/`;
+
+class API {
+  constructor(baseURL, timeout) {
+    this._http = axios.create({
+      baseURL,
+      timeout
+    });
+  }
+
+  async _load(url, options) {
+    const response = await this._http.request({url, ...options});
+    return response.data;
+  }
+
+  getPosts() {
+    return this._load(`/articles`);
+  }
+
+  getPost(id) {
+    return this._load(`/articles/${id}`);
+  }
+
+  getCategories() {
+    return this._load(`/categories`);
+  }
+
+  search(query) {
+    return this._load(`/search`, {params: query});
+  }
+}
+
+const defaultAPI = new API(defaultURL, TIMEOUT);
+
+module.exports = {
+  API,
+  getAPI: () => defaultAPI
+};
