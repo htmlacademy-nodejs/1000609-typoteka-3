3267c2c27466d68b8204677c270f46b89b75c825
Ilya Volkov <volkov-ilyha@ya.ru>
2021-07-08 22:28:33 +0300

    🌐 front-server: add data loading in /articles/edit/:articleId route

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -1,12 +1,21 @@
 'use strict';
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
+const {formatDate, formatDatetime} = require(`../../utils`);
 
 const articlesRouter = new Router();
 
 articlesRouter.get(`/add`, (req, res) => res.render(`articles/new-post`));
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles/articles-by-category`));
 articlesRouter.get(`/:id`, (req, res) => res.render(`articles/post`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.render(`articles/post`));
+articlesRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [post, categories] = await Promise.all([
+    api.getPost(id),
+    api.getCategories()
+  ]);
+  res.render(`articles/post`, {post, categories, formatDate, formatDatetime});
+});
 
 module.exports = articlesRouter;
