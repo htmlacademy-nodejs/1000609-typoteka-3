c6a85225db57e3713a80d3b48e7863830fa17d88
Ilya Volkov <volkov-ilyha@ya.ru>
2021-09-14 23:01:24 +0300

    🔧 post-validator refactor

--- src/service/middlewares/post-validator.js
+++ src/service/middlewares/post-validator.js
@@ -4,35 +4,43 @@ const Joi = require(`joi`);
 const {HttpCode} = require(`../../constants`);
 
 const ErrorPostMessage = {
-  TITLE: `Заголовок - обязательное поле. Минимум 30 символов. Максимум 250.`,
+  TITLE: `Заголовок - обязательное поле.`,
+  TITLE_MIN: `Заголовок - минимум 30 символов.`,
+  TITLE_MAX: `Заголовок - максимум 250 символов.`,
   CREATED_AT: `Дата публикации - обязательное поле.`,
   PICTURE: `Фотография - доступны форматы jpg и png.`,
-  ANNOUNCEMENT: `Анонс публикации - обязательное поле. Минимум 30 символов. Максимум 250.`,
+  ANNOUNCEMENT: `Анонс публикации - обязательное поле.`,
+  ANNOUNCEMENT_MIN: `Анонс публикации - минимум 30 символов.`,
+  ANNOUNCEMENT_MAX: `Анонс публикации - максимум 250.`,
   FULL_TEXT: `Полный текст публикации - максимум 1000 символов.`,
   CATEGORIES: `Категория - для выбора обязательна хотя бы одна категория.`,
 };
 
 const schema = Joi.object({
   title: Joi.string().min(30).max(250).required().messages({
-    'any.invalid': ErrorPostMessage.TITLE
+    'any.required': ErrorPostMessage.TITLE,
+    'string.min': ErrorPostMessage.TITLE_MIN,
+    'string.max': ErrorPostMessage.TITLE_MAX,
   }),
   createdAt: Joi.string().isoDate().required().messages({
-    'any.invalid': ErrorPostMessage.CREATED_AT
+    'any.required': ErrorPostMessage.CREATED_AT
   }),
-  picture: Joi.string().pattern(/\.(?:jpg|png)$/i).messages({
-    'any.invalid': ErrorPostMessage.PICTURE
+  picture: Joi.string().pattern(/\.(?:jpg|png)$/i).allow(null).messages({
+    'string.pattern.base': ErrorPostMessage.PICTURE,
   }),
   announcement: Joi.string().min(30).max(250).required().messages({
-    'any.invalid': ErrorPostMessage.ANNOUNCEMENT
+    ...([`any.required`, `string.empty`].reduce((result, rule) => ({...result, [rule]: ErrorPostMessage.ANNOUNCEMENT}), {})),
+    'string.min': ErrorPostMessage.ANNOUNCEMENT_MIN,
+    'string.max': ErrorPostMessage.ANNOUNCEMENT_MAX,
   }),
-  fullText: Joi.string().max(1000).messages({
-    'any.invalid': ErrorPostMessage.FULL_TEXT
+  fullText: Joi.string().max(1000).allow(null).messages({
+    'string.max': ErrorPostMessage.FULL_TEXT,
   }),
   categories: Joi.array().min(1).items(
       Joi.number().integer().positive()
-  ).required().messages({
-    'any.invalid': ErrorPostMessage.CATEGORIES
-  })
+  ).required().messages(
+      [`any.required`, `array.min`].reduce((result, rule) => ({...result, [rule]: ErrorPostMessage.CATEGORIES}), {})
+  )
 });
 
 module.exports = (req, res, next) => {
@@ -41,7 +49,10 @@ module.exports = (req, res, next) => {
 
   if (error) {
     return res.status(HttpCode.BAD_REQUEST)
-      .send(error.details.map((err) => err.message));
+      .send(error.details.map((err) => ({
+        field: err.context.key,
+        message: err.message
+      })));
   }
 
   return next();
