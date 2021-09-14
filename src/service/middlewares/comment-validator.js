62a7cb816200ca192c34300334a4bec2467c919a
Ilya Volkov <volkov-ilyha@ya.ru>
2021-09-14 17:58:07 +0300

    🛠 add joi dependence and upgrades comment-validator

--- src/service/middlewares/comment-validator.js
+++ src/service/middlewares/comment-validator.js
@@ -1,17 +1,27 @@
 'use strict';
 
+const Joi = require(`joi`);
 const {HttpCode} = require(`../../constants`);
 
-const commentKeys = [`text`];
+const ErrorCommentMessage = {
+  MIN: `Комментарий содержит меньше 20 символов`,
+  REQUIRED: `Текст комментария - обязательное поле`
+};
+
+const schema = Joi.object({
+  text: Joi.string().min(20).required().messages({
+    'string.min': ErrorCommentMessage.MIN,
+    'any.required': ErrorCommentMessage.REQUIRED
+  })
+});
 
 module.exports = (req, res, next) => {
   const comment = req.body;
-  const keys = Object.keys(comment);
-  const keysExists = commentKeys.every((key) => keys.includes(key));
 
-  if (!keysExists) {
+  const {error} = schema.validate(comment, {abortEarly: false});
+  if (error) {
     return res.status(HttpCode.BAD_REQUEST)
-      .send(`Bad request`);
+      .send(error.details.map((err) => err.message));
   }
 
   return next();
