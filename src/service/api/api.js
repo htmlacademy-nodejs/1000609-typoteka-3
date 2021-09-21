2ac8e8cf021c09feb8156462191f4b8cf740111a
Ilya Volkov <volkov-ilyha@ya.ru>
2021-09-21 23:09:52 +0300

    🧶 add user route to server api

--- src/service/api/api.js
+++ src/service/api/api.js
@@ -6,11 +6,13 @@ const defineModels = require(`../models/defineModels`);
 const category = require(`../api/category`);
 const post = require(`../api/post`);
 const search = require(`../api/search`);
+const user = require(`../api/user`);
 const {
   CategoryService,
   SearchService,
   PostService,
   CommentService,
+  UserService
 } = require(`../data-service`);
 
 const getRoutes = async () => {
@@ -21,6 +23,7 @@ const getRoutes = async () => {
   category(app, new CategoryService(sequelize));
   search(app, new SearchService(sequelize));
   post(app, new PostService(sequelize), new CommentService(sequelize));
+  user(app, new UserService(sequelize));
 
   return app;
 };
