3a5dd15191ecd6041f527034ebe69b64a8c5273b
Ilya Volkov <volkov-ilyha@ya.ru>
2021-09-21 22:23:48 +0300

    📋 creating user data service

--- src/service/data-service/index.js
+++ src/service/data-service/index.js
@@ -4,10 +4,12 @@ const PostService = require(`./post`);
 const CommentService = require(`./comment`);
 const SearchService = require(`./search`);
 const CategoryService = require(`./category`);
+const UserService = require(`./user`);
 
 module.exports = {
   PostService,
   CommentService,
   SearchService,
-  CategoryService
+  CategoryService,
+  UserService
 };
