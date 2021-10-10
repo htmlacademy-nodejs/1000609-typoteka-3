d50d4a4633e397f79be2d9fbb090cd7ec808c3c3
Ilya Volkov <volkov-ilyha@ya.ru>
2021-10-10 15:10:08 +0300

    🔧 my-routes refactor

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -6,12 +6,13 @@ const {formatDate, formatDatetime} = require(`../../utils`);
 const api = require(`../api`).getAPI();
 
 const myRouter = new Router();
+myRouter.use(auth(true));
 
-myRouter.get(`/`, auth(true), async (req, res) => {
+myRouter.get(`/`, async (req, res) => {
   const posts = await api.getPosts();
   res.render(`user/my`, {posts, formatDate, formatDatetime});
 });
-myRouter.get(`/comments`, auth(true), async (req, res) => {
+myRouter.get(`/comments`, async (req, res) => {
   const posts = await api.getPosts({comments: true});
   res.render(`user/comments`, {posts: posts.slice(0, 3), formatDate, formatDatetime});
 });
