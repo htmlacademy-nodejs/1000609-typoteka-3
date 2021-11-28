015b4031bbd3f01a4b3bf2dd1000688d17156667
Ilya Volkov <volkov-ilyha@ya.ru>
2021-11-28 17:12:36 +0300

    🌐 front-server: update post template

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -29,10 +29,12 @@ articlesRouter.get(`/add`, auth(true), csrfProtection, async (req, res) => {
 articlesRouter.post(`/add`, auth(true), upload.single(`upload`), csrfProtection, async (req, res) => {
   const {body, file} = req;
   const {user} = req.session;
+  const hours = new Date().getHours().toString().padStart(2, `0`);
+  const minutes = new Date().getMinutes().toString().padStart(2, `0`);
 
   const postData = {
     title: body.title,
-    createdAt: `${body.date}T00:00:00.000Z`,
+    createdAt: `${body.date}T${hours}:${minutes}:00.000Z`,
     picture: file && file.filename ? file.filename : (body.photo || null),
     announcement: body.announcement,
     fullText: body[`full-text`] ? body[`full-text`] : null,
@@ -102,10 +104,12 @@ articlesRouter.post(`/edit/:id`, auth(true), upload.single(`upload`), csrfProtec
   const {body, file} = req;
   const {id} = req.params;
   const {user} = req.session;
+  const hours = new Date().getHours().toString().padStart(2, `0`);
+  const minutes = new Date().getMinutes().toString().padStart(2, `0`);
 
   const postData = {
     title: body.title,
-    createdAt: `${body.date}T00:00:00.000Z`,
+    createdAt: `${body.date}T${hours}:${minutes}:00.000Z`,
     picture: file && file.filename ? file.filename : (body.photo || null),
     announcement: body.announcement,
     fullText: body[`full-text`] ? body[`full-text`] : null,
